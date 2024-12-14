5256dbe9e975a517b23b73abfeeb0d26f93757e5
Brandeau <diego.andrade.y@gmail.com>
2024-12-14 16:13:42 -0300

    feat: create file with enemies

---
+++ server/data/enemies.js
@@ -0,0 +1,100 @@
+const enemies = {
+        noel: {
+                name: "Noel R. Skum",
+                health: 10,
+                moveset: [{
+                        0:{
+                                name: "Subsidy Slash",
+                                description: "becomes richer through subsidies and you lose welfare support",
+                                power: 1
+                        },
+                        1:{
+                                name: "Foreign Asset Beam",
+                                description: "receives shady money from other nations, you lose sovergnity",
+                                power: 2
+                        },
+                        2:{
+                                name: "Social Media Punch",
+                                description: "buys an entire social media site to promote misinformation, you become confused",
+                                power: 2
+                        },
+
+                        3: {
+                                name: "Rocket Boom",
+                                description: "privatizes space exploration, you are trapped on a dying Earth",
+                                power: 3
+                        },
+
+                        4: {
+                                name: "Brainwave Shock",
+                                description: "plugs your brain to a neural net, you succumb to his mind control",
+                                power: 3
+                        }
+                }]
+        },
+
+        joff:{
+                name: "Joffrey Amazos",
+                health: 10,
+                moveset: [{
+                        0: {
+                                name: "Monopoly Mash",
+                                description: "monopolizes the industry and your wages go down",
+                                power: 1
+                        },
+                        1: {
+                                name: "Big Brother Bomb",
+                                description: "Increasing camera surveillance forces you to skip bathroom breaks to meet your quota; you develop urinary problems",
+                                power: 2
+                        },
+                        2: {
+                                name: "Ball and chain",
+                                description: "moves production abroad; you are now trapped in a force labor camp producing for him",
+                                power: 2
+                        },
+                        3: {
+                                name: "Scorched Earth",
+                                description: "eludes environmental laws increasing pollution and giving you cancer",
+                                power: 3
+                        },
+                        4: {
+                                name: "Misinfo Dump",
+                                description: "publishes books with misinformation; you've become confused",
+                                power: 3
+                        }
+                        
+                }]
+        },
+        dolan:{
+                name: "Dolan Rump",
+                health: 10,
+                moveset: [{
+                        0: {
+                                name: "Bailout Bazooka",
+                                description:"bankrupts the company and is bailed out by the government. There's less money for your welfare",
+                                power: 1
+                        },
+                        1:{
+                                name: "Assassination Evation",
+                                description: "survives an assassination attempt and becomes president",
+                                power: 3
+                        },
+                        2:{
+                                name: "The Apprentice",
+                                description: "drags you into a survival reality tv show and fires you",
+                                power: 2
+                        },
+                        3:{
+                                name: "Treason Trump Card",
+                                description: "sells your location to foreign governments. You are targeted by black ops",
+                                power: 3
+                        },
+                        4:{
+                                name: "Stochastic Terrorism",
+                                description: "pushes violent rethoric publicly about your family. You are targeted by his following",
+                                power: 2
+                        }
+                }]
+        }
+}
+
