a040beba770a3779497631862224a756a4a3936f
Brandeau <diego.andrade.y@gmail.com>
2024-12-14 16:43:50 -0300

    feat: create file with initial clashers

---
+++ server/data/clashers.js
@@ -0,0 +1,64 @@
+const clashers = {
+    luigi: {
+        name: "Luigi",
+        health: 10,
+        moveset: [{
+            0: {
+                name: "Vigilate Punch",
+                description: "",
+                power: 3
+            },
+            1: {
+                name: "Labor Inspection Complaint",
+                description: "",
+                power: 1
+            },
+            2: {
+                name: "Silent Quitting",
+                description: "",
+                power: 2
+            },
+            3: {
+                name: "Industrial Sabotage",
+                description: "",
+                power: 3
+            },
+            4: {
+                name: "DDOS Attack",
+                description: "",
+                power: 2
+            }
+        }]
+    },
+    tracy: {
+        name: "Tracy",
+        health: 10,
+        moveset: [{
+            0: {
+                name: "Class Action Lawsuit",
+                description: "",
+                power: 2
+            },
+            1: {
+                name: "Egg Throw",
+                description: "",
+                power: 1
+            },
+            2: {
+                name: "Fire Punch",
+                description: "",
+                power: 2
+            },
+            3: {
+                name: "Underwater Extravaganza",
+                description: "Throws enemy in submarine which implodes",
+                power: 3
+            },
+            4: {
+                name: "Cancellation",
+                description: "cancels the enemy through social media",
+                power: 3
+            }
+        }]
+    }
+}
