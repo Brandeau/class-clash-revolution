bc5b0cdfae0cbacfc28d8b25df3a4670a43f367e
Brandeau <diego.andrade.y@gmail.com>
2024-12-18 13:37:37 -0300

    refactor: change to async await

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -6,20 +6,27 @@ const usernameForm = document.getElementById('username-form');
  * 
  * @param {SubmitEvent} event 
  */
-function createBasePlayer(event){
-    event.preventDefault();
+async function createBasePlayer(event){
+        event.preventDefault();
 
-    const nickname = event.target?.username?.value;
-    
-    fetch("http://localhost:3000/api/players", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: nickname,
-        })
-    }).then((r) => r.json()).then(console.log).catch(console.error);
+        const nickname = event.target?.username?.value;
+        
+        try{
+            const response = await fetch("http://localhost:3000/api/players", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: nickname,
+                })
+            });
+            const playerData = await response.json();
+            console.log(playerData)
+
+        } catch(e){
+            console.error(e)
+        }
 }
 
 usernameForm.addEventListener('submit', createBasePlayer)
