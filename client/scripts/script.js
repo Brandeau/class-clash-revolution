58b5885d9dc1a43f3a9dbc830561194dd90abbb4
Brandeau <diego.andrade.y@gmail.com>
2024-12-19 14:44:35 -0300

    feat: add functionality to fetch dynamic player route

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -1,6 +1,9 @@
 
 const startButton = document.getElementById('start-button');
 const usernameForm = document.getElementById('username-form');
+const characterForm = document.getElementById('character-form');
+
+let player;
 
 /**
  * 
@@ -22,11 +25,41 @@ async function createBasePlayer(event){
                 })
             });
             const playerData = await response.json();
-            console.log(playerData)
+            player = playerData;
+            getPlayer(player.id)    
+            console.log(player)
 
         } catch(e){
             console.error(e)
         }
 }
 
-usernameForm.addEventListener('submit', createBasePlayer)
+async function getPlayer(playerId){
+    try{
+        const response = await fetch(`http://localhost:3000/api/players/${playerId}`);
+        const data = await response.json();
+        console.log(data);
+        
+    } catch(e){
+        console.error("Error getting player:", e)
+    }
+}
+
+async function chooseClasher(event){
+    event.preventDefault();
+    
+    const selectedChar = characterForm.elements.character.value;
+    try{
+        const response = await fetch('http://localhost:3000/api/clashers');
+        const data = await response.json();
+        player.clashers.push(data[selectedChar])
+        console.log(player.clashers)
+        console.log(data["luigi"])
+
+    } catch(e){
+
+    }
+}
+
+usernameForm.addEventListener('submit', createBasePlayer);
+characterForm.addEventListener('submit', chooseClasher);
